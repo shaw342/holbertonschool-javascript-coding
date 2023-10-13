eb132a47446b1c8ce7fc1e80c0d71f9c4d35620f
VotreNom <shawanbarua70@gmail.com>
2023-10-13 17:48:09 +0200

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log'Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
